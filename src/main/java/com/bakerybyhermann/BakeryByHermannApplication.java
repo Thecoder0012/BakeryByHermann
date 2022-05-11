b960c249adefc43a60e8edc9f5f980bee0c00d91
Mardin <mardineliassi57@gmail.com>
2022-05-11 22:03:03 +0200

    Starting the spring project!

---
+++ src/main/java/com/bakerybyhermann/BakeryByHermannApplication.java
@@ -0,0 +1,13 @@
+package com.bakerybyhermann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryByHermannApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakeryByHermannApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bakerybyhermann/BakeryByHermannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakerybyhermann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryByHermannApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
