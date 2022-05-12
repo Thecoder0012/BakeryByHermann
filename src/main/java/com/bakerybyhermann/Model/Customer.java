c282c9300508672dedb99b9e951b1e67e0df4005
Mardin <mardineliassi57@gmail.com>
2022-05-12 10:48:35 +0200

    Customer Model created.

---
+++ src/main/java/com/bakerybyhermann/Model/Customer.java
@@ -0,0 +1,33 @@
+package com.bakerybyhermann.Model;
+
+public class Customer extends Person {
+
+    private int repeatedVisits;
+    private String companyName;
+
+
+    public Customer(String firstName, String lastName, String address, int zipCode, String city, int phoneNumber, String email, int repeatedVisits, String companyName) {
+        super(firstName, lastName, address, zipCode, city, phoneNumber, email);
+        this.repeatedVisits = repeatedVisits;
+        this.companyName = companyName;
+    }
+
+    public Customer() {
+    }
+
+    public int getRepeatedVisits() {
+        return repeatedVisits;
+    }
+
+    public void setRepeatedVisits(int repeatedVisits) {
+        this.repeatedVisits = repeatedVisits;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
