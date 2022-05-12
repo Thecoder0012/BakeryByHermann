2caa353e79662955f72e168806e9b4f0fd083337
Mardin <mardineliassi57@gmail.com>
2022-05-12 10:40:35 +0200

    Person Model created.

--- src/main/java/com/bakerybyhermann/Model/Person.java
+++ src/main/java/com/bakerybyhermann/Model/Person.java
@@ -1,4 +1,81 @@
 package com.bakerybyhermann.Model;
 
 public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int zipCode;
+    private String city;
+    private int phoneNumber;
+    private String email;
+
+    public Person(String firstName, String lastName, String address, int zipCode, String city, int phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
