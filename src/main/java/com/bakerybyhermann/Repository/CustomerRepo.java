d139d35a852702c9f83ede5338ea15d898e6e84d
Mardin <mardineliassi57@gmail.com>
2022-05-19 00:26:37 +0200

    Update in progress.. 66%

--- src/main/java/com/bakerybyhermann/Repository/CustomerRepo.java
+++ src/main/java/com/bakerybyhermann/Repository/CustomerRepo.java
@@ -26,16 +26,15 @@ public class CustomerRepo {
 //                "                JOIN zip_code_tbl ON address_tbl.zip_code = zip_code_tbl.zip_code";
 
 //        String sql1 = "SELECT * FROM address_tbl";
-        String sql = "SELECT customer_id as personId,first_name,last_name," +
-
-                "street_name as streetName,street_number as streetNumber,address_tbl.zip_code,city," +
-                //disse skal i object for sig knyttet til denne person object
-
+        String sql = "SELECT customer_id as personId,first_name,last_name," + "street_name as streetName,street_number as streetNumber,address_tbl.zip_code,city," +
                 "phone_number,e_mail as email,repeated_visits,company_name\n" +
                 "FROM person_tbl\n" +
                 "INNER JOIN customer_tbl ON person_tbl.person_id = customer_tbl.person_id\n" +
                 "INNER JOIN address_tbl ON person_tbl.address_id = address_tbl.address_id\n" +
                 "INNER JOIN  zip_code_tbl ON address_tbl.zip_code = zip_code_tbl.zip_code";
+
+        //disse skal i object for sig knyttet til denne person object
+
 
 //       // String sql = "SELECT customer_tbl.customer_id as personId,first_name,last_name,CONCAT(street_name, ' ', street_number) as address,address_tbl.zip_code,city,phone_number,e_mail as email,repeated_visits,company_name\n" + "FROM person_tbl\n" + "INNER JOIN customer_tbl ON person_tbl.person_id = customer_tbl.person_id\n" + "INNER JOIN address_tbl ON person_tbl.address_id = address_tbl.address_id\n" + "INNER JOIN  zip_code_tbl ON address_tbl.zip_code = zip_code_tbl.zip_code";
 //        String sql = "SELECT * FROM person_tbl " +
@@ -111,11 +110,7 @@ public class CustomerRepo {
 
     }
 
-    public Customer findById(int id) {
-        String sql = "SELECT customer_id as personId, first_name, last_name, CONCAT(street_name, ' ', street_number) as address, zip_code_tbl.zip_code, city, phone_number, e_mail as email, repeated_visits, company_name\n" + "   FROM customer_tbl \n" + "                JOIN address_tbl  ON  address_tbl.address_id = customer_tbl.address_id \n" + "                JOIN zip_code_tbl ON address_tbl.zip_code = zip_code_tbl.zip_code WHERE customer_id = ?";
-        RowMapper<Customer> rowMapper = new BeanPropertyRowMapper<>(Customer.class);
-        return jdbcTemplate.queryForObject(sql, rowMapper, id);
-    }
+
 
     public void updateById(int id, Customer c) {
         /*String address = c.getAddress(); //her bruger vi input fra Html
@@ -125,25 +120,87 @@ public class CustomerRepo {
         String streetName = splitted[0]; // her her vi gadenavnet*/
 
 
-        String sql1 = "UPDATE address_tbl SET street_name = ?, street_number = ?, zip_code = ? WHERE address_id = ?";
-        jdbcTemplate.update(sql1, c.getAddress().getStreetName(), c.getAddress().getStreetNumber(), c.getAddress().getZipCode(), getUpdateAddressId(id));
+        String sqlAddress = "UPDATE address_tbl SET street_name = ?, street_number = ?, zip_code = ? WHERE address_id = ?";
+//        System.out.println(c.getAddress().getStreetName());
+        System.out.println("id is 45 " +c.getAddress().getAddressId());
+        System.out.println("streetname must be : afk " +c.getAddress().getStreetName());
 
 
 
-        String sql = "UPDATE customer_tbl SET customer_id = ?, first_name = ?, last_name = ?, address_id = ?, phone_number = ?," + "e_mail = ?, repeated_visits = ?, company_name = ? WHERE customer_id = ?";
-        jdbcTemplate.update(sql, c.getPersonId(), c.getFirstName(), c.getLastName(), getUpdateAddressId(id), c.getPhoneNumber(), c.getEmail(), c.getRepeatedVisits(), c.getCompanyName(), id);
+        jdbcTemplate.update(sqlAddress, c.getAddress().getStreetName(), c.getAddress().getStreetNumber(), c.getAddress().getZipCode(), 44);
+        System.out.println(c.getAddress().getAddressId());
+        System.out.println("til her");
+        String sqlCustomer = "UPDATE customer_tbl SET repeated_visits = ?, company_name = ? WHERE customer_id = ?";
+        jdbcTemplate.update(sqlCustomer, c.getRepeatedVisits(), c.getCompanyName(), id);
+
+        System.out.println("før"+id);
+
+        String sqlPerson = "UPDATE person_tbl SET first_name = ?, last_name = ?, phone_number = ?,e_mail = ? WHERE person_tbl.person_id = ?";
+        jdbcTemplate.update(sqlPerson, c.getFirstName(), c.getLastName(), c.getPhoneNumber(),c.getEmail(), id);
+        System.out.println("efter"+id);
+
+        System.out.println("id isssss 45 " +getUpdateAddressId(id));
+
+
+
+
+
+
+
+
+
     }
 
     public int getUpdateAddressId(int id) {
-        String sql = "SELECT address_id FROM customer_tbl WHERE customer_id = ?";
+        String sql = "SELECT person_tbl.address_id, street_name,street_number,zip_code_tbl.zip_code,city FROM person_tbl \n" +
+                "INNER JOIN address_tbl ON person_tbl.address_id = address_tbl.address_id \n" +
+                "INNER JOIN zip_code_tbl ON address_tbl.zip_code = zip_code_tbl.zip_code\n" +
+                "WHERE person_id = ?";
         RowMapper<Address> rowMapper = new BeanPropertyRowMapper<>(Address.class);
         Address address = jdbcTemplate.queryForObject(sql, rowMapper, id);
+        System.out.println("new debug get object address id"+address.getAddressId());
         return address.getAddressId();
     }
 
+    public Customer findById(int id) {
+//        String sql = "SELECT customer_id as personId, first_name, last_name, " +
+//                "street_name as streetName,street_number as streetNumber, " +
+//                "address_tbl.zip_code, phone_number, e_mail as email, repeated_visits, company_name\n" + "  " +
+//                " FROM person_tbl \n" + "  " +
+//                " INNER JOIN customer_tbl ON person_tbl.person_id = customer_tbl.person_id \n" + " " +
+//                " INNER JOIN address_tbl on person_tbl.address_id = address_tbl.address_id WHERE customer_id = ?";
+//
+
+        String sql = "SELECT customer_id as personId,first_name,last_name," + "street_name as streetName,street_number as streetNumber,address_tbl.zip_code,city," +
+                "phone_number,e_mail as email,repeated_visits,company_name\n" +
+                "FROM person_tbl\n" +
+                "INNER JOIN customer_tbl ON person_tbl.person_id = customer_tbl.person_id\n" +
+                "INNER JOIN address_tbl ON person_tbl.address_id = address_tbl.address_id\n" +
+                "INNER JOIN  zip_code_tbl ON address_tbl.zip_code = zip_code_tbl.zip_code WHERE customer_id = ?";
+
+        //disse skal i object for sig knyttet til denne person object
+
+
+//       // String sql = "SELECT customer_tbl.customer_id as personId,first_name,last_name,CONCAT(street_name, ' ', street_number) as address,address_tbl.zip_code,city,phone_number,e_mail as email,repeated_visits,company_name\n" + "FROM person_tbl\n" + "INNER JOIN customer_tbl ON person_tbl.person_id = customer_tbl.person_id\n" + "INNER JOIN address_tbl ON person_tbl.address_id = address_tbl.address_id\n" + "INNER JOIN  zip_code_tbl ON address_tbl.zip_code = zip_code_tbl.zip_code";
+//        String sql = "SELECT * FROM person_tbl " +
+//                "     INNER JOIN customer_tbl " +
+//                "     ON person_tbl.person_id = customer_tbl.customer_id" +
+//                "       ";
+        RowMapper rowMapper = new CustomerMapper();
+
+        List<Customer> customerList = jdbcTemplate.query(sql,rowMapper,id);
+
+        return customerList.get(0);
+    }
 
     public void delete(int customerId) {
-        String sql = "DELETE customer_tbl,address_tbl FROM customer_tbl " + "INNER JOIN address_tbl ON customer_tbl.address_id = address_tbl.address_id WHERE customer_id = ?";
+        String sql = "DELETE customer_tbl,person_tbl,address_tbl FROM customer_tbl \n" +
+                "INNER JOIN person_tbl ON customer_tbl.person_id = person_tbl.person_id \n" +
+                "INNER JOIN address_tbl on person_tbl.address_id = address_tbl.address_id\n" +
+                "WHERE customer_id = ?";
+
+
+
         jdbcTemplate.update(sql, customerId);
     }
 
