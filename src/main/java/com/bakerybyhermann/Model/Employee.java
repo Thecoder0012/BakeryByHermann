1332f6d78a09d4ba52f9f731b97843b743e55a62
Mardin <mardineliassi57@gmail.com>
2022-05-19 12:42:57 +0200

    Employee, cashier changes.

--- src/main/java/com/bakerybyhermann/Model/Employee.java
+++ src/main/java/com/bakerybyhermann/Model/Employee.java
@@ -1,35 +1,20 @@
 package com.bakerybyhermann.Model;
 
 public class Employee extends Person {
-/*
+
+    private int employeeId;
     private int age;
     private boolean gender;
     private boolean fullTimeEmployee;
 
-    public Employee(int personId, String firstName, String lastName, Address address, int phoneNumber, String email, int repeatedVisits, String companyName, int age, boolean gender) {
-        super(personId, firstName, lastName, address,  phoneNumber, email, repeatedVisits, companyName);
+    public Employee(int personId, String firstName, String lastName, Address address, int phoneNumber, String email, int employeeId, int age, boolean gender, boolean fullTimeEmployee) {
+        super(personId, firstName, lastName, address, phoneNumber, email);
+        this.employeeId = employeeId;
         this.age = age;
         this.gender = gender;
+        this.fullTimeEmployee = fullTimeEmployee;
     }
 
     public Employee() {
-
     }
-
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public boolean isGender() {
-        return gender;
-    }
-
-    public void setGender(boolean gender) {
-        this.gender = gender;
-    }*/
 }
