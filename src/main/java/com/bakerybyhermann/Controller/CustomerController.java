36997a0c1a7a4e4d9f8934371d22c29e199a3403
Unknown1321 <arenstorffenterprise@gmail.com>
2022-05-23 11:15:42 +0200

    Removed SOUT from CustomerController

--- src/main/java/com/bakerybyhermann/Controller/CustomerController.java
+++ src/main/java/com/bakerybyhermann/Controller/CustomerController.java
@@ -45,19 +45,13 @@ public class CustomerController {
     @GetMapping("/update-customer/{id}")
     public String updateCustomer(@PathVariable("id") int id, Model model){
         Customer customer = customerService.findById(id);
-        System.out.println("get update customer id is: "+id);
-        System.out.println(customer.getCompanyName());
         model.addAttribute("customer", customer);
         return "customer/update-customer";
     }
 
     @PostMapping("/update-customer")
     public String updateCustomer(@ModelAttribute Customer customer){
-        System.out.println(customer.getAddress().getAddressId());
-        System.out.println(customer.getAddress().getStreetName());
-        System.out.println(customer.getAddress().getCity());
         customerService.updateById(customer.getPersonId(), customer);
-        System.out.println("id er lig med  ="+customer.getPersonId());
         return "redirect:/";
     }
 }
