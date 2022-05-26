b8b3e2202e50fe491b9567f1c962b342610c4646
Mardin <mardineliassi57@gmail.com>
2022-05-26 20:16:44 +0200

    - Redirecting to the same url when updating an employee, now you have the choice to click the "tilbage" button instead of being redirected when doing a post request.

--- src/main/java/com/bakerybyhermann/Controller/DriverController.java
+++ src/main/java/com/bakerybyhermann/Controller/DriverController.java
@@ -7,10 +7,9 @@ import com.bakerybyhermann.Service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class DriverController {
@@ -19,45 +18,46 @@ public class DriverController {
     DriverService driverService;
 
     @GetMapping("/driver")
-    public String getDriver(Model model){
-        model.addAttribute("driversList",driverService.fetchAll());
+    public String getDriver(Model model) {
+        model.addAttribute("driversList", driverService.fetchAll());
         return "driver/view-driver";
     }
 
     @GetMapping("/new-driver")
-    public String createDriver(){
+    public String createDriver() {
         return "driver/new-driver";
     }
 
     @PostMapping("/new-driver")
-    public String createDriver(@ModelAttribute Driver driver, @ModelAttribute Address address){
+    public String createDriver(@ModelAttribute Driver driver, @ModelAttribute Address address) {
         driver.setAddress(address);
         driverService.addNewDriver(driver, address);
         return "redirect:/driver";
     }
 
     @GetMapping("/update-driver/{driverId}")
-    public String updateDriver(@PathVariable("driverId") int id, Model model){
+    public String updateDriver(@PathVariable("driverId") int id, Model model) {
         Driver driver = driverService.findById(id);
         model.addAttribute("driver", driver);
         return "driver/update-driver";
     }
 
-    @PostMapping("/update-driver")
-    public String updateDriver(@ModelAttribute Driver driver){
+    @PostMapping(value = "/update-driver")
+    public String updateDriver(@ModelAttribute Driver driver, HttpServletRequest request) {
+        String referer = request.getHeader("Referer");
         driverService.updateById(driver);
-        return "redirect:/driver";
+        return "redirect:" + referer;
     }
 
     @GetMapping("/driver/{driverId}")
-    public String deleteDriver(@PathVariable("driverId") int driverId){
+    public String deleteDriver(@PathVariable("driverId") int driverId) {
         driverService.deleteDriver(driverId);
         return "redirect:/view-driver";
     }
 
 
     @GetMapping("/view-driver/{oneDriver}")
-    public String viewOne(@PathVariable("oneDriver") int id, Model model){
+    public String viewOne(@PathVariable("oneDriver") int id, Model model) {
         model.addAttribute("oneDriver", driverService.findById(id));
         return "driver/one-driver";
     }
