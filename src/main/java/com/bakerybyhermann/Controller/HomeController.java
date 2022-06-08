5db2f07eed5da77637b97e87c65540ca723b5eb2
Mardin <mardineliassi57@gmail.com>
2022-06-08 23:21:35 +0200

    - Fixing button in create order view.

--- src/main/java/com/bakerybyhermann/Controller/HomeController.java
+++ src/main/java/com/bakerybyhermann/Controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.bakerybyhermann.Controller;
 
-import com.bakerybyhermann.Service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
+//import com.bakerybyhermann.Service.CustomerService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+//import org.springframework.security.authentication.AnonymousAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +20,21 @@ public class HomeController {
         return "home/index";
     }
 
+
+
     @GetMapping("/login")
     public String getLogin(){
         return "home/login";
     }
 
+    @GetMapping("/login")
+    public String authenticated(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || authentication instanceof AnonymousAuthenticationToken){
+            return "home/login";
 
+        }
+        return "redirect:/";
+    }
 
 }
